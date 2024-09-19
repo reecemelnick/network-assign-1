a0586200264dbeae0c108018ff42a1b9d10f49b5
reecemelnick <reececianmelnick@gmail.com>
2024-09-19 09:20:37 -0700

    added parse func

--- main.c
+++ main.c
@@ -4,15 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+int const LOWERCASE_MIN = 96;
+int const LOWERCASE_MAX = 123;
+int const DISTANCE_TO_OTHER = 32;
+int const UPPERCASE_MIN = 64;
+int const UPPERCASE_MAX = 91;
+
 char upper_filter(char c) {
-    if(c > 96 & c < 123) {
+    if(c > LOWERCASE_MIN & c < LOWERCASE_MAX) {
         c -= 32;
     }
     return c;
 }
 
 char lower_filter(char c) {
-    if(c > 64 & c < 91) {
+    if(c > UPPERCASE_MIN & c < UPPERCASE_MAX) {
         c += 32;
     }
     return c;
@@ -30,6 +36,47 @@ char* transform(char *c, char (*transformation)(char)) {
     return c;
 }
 
+char** parse_args(int argc, char** argv) {
+    int chara;
+    char *input_file;
+    char *output_file;
+    char *result;
+    char (*transform_func)(char);
+    char** results = (char**)malloc(sizeof(char*) * 3);
+    results[0] = (char*)malloc(sizeof(argv[0]));
+    results[1] = (char*)malloc(sizeof(argv[1]));
+    results[2] = (char*)malloc(sizeof(argv[3]));
+
+    while ((chara = getopt (argc, argv, "i:o:f:")) != -1) {
+        switch (chara) {
+            case 'i':
+                input_file = optarg;
+                results[0] = input_file;
+                break;
+            case 'o':
+                output_file = optarg;
+                results[1] = output_file;
+            case 'f':
+                result = optarg;
+                if (strcmp(result, "upper") == 0) {
+                    transform_func = upper_filter;
+                } else if (strcmp(result, "lower") == 0) {
+                    transform_func = lower_filter;
+                } else {
+                    transform_func = null_filter;
+                }
+                results[2] = result;
+                break;
+            default:
+                NULL;
+        }
+    }
+
+    return results;
+}
+
+
+
 int main(int argc, char *argv[]) {
 
     int chara;
@@ -37,6 +84,12 @@ int main(int argc, char *argv[]) {
     char *output_file;
     char *result;
     char (*transform_func)(char);
+
+//    char** argument_strings = parse_args(argc, argv);
+//    input_file = argument_strings[0];
+//    output_file = argument_strings[1];
+
+//    printf("arg 1: %s", argument_strings[0]);
 
     while ((chara = getopt (argc, argv, "i:o:f:")) != -1) {
         switch (chara) {
@@ -68,6 +121,7 @@ int main(int argc, char *argv[]) {
     ssize_t sz;
     while((sz = read(fd, c, 1)) > 0) {
         c = transform(c, transform_func);
+//        c = transform(c, upper_filter);
         ssize_t sz2 = write(fd2, c, 1);
     }
 
